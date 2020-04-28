5d87b862b3fe3dea326cede4c252176ddd042c53
moakprojects <molak8@gmail.com>
2020-04-28 21:20:15 +0200

    Add files via upload

---
+++ MathCompiler.java
@@ -0,0 +1,8 @@
+package math.compiler;
+
+public class MathCalculation {
+	public void calculate() {
+		System.out.println("first expression = " + 18);
+		System.out.println("second expression = " + 24);
+	}
+}
